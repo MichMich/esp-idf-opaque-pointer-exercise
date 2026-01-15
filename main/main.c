f56b31ab115eac12530b7fada42da936d07f369c
Michael Teeuw <michael@xonaymedia.nl>
2026-01-15 14:26:41 +0100

    Clean up.

--- main/main.c
+++ main/main.c
@@ -1,16 +1,15 @@
-#include <stdio.h>
-#include "freertos/FreeRTOS.h"
 #include "scheduled_logger.h"   
 
 void app_main(void)
 {
+    // Create and start two scheduled loggers with different messages and intervals:
     scheduled_logger_handle_t logger1 = scheduled_logger_create("Hello, Scheduled Logger every sec!", 1000);
     scheduled_logger_start(logger1);
 
     scheduled_logger_handle_t logger2 = scheduled_logger_create("Hello, Scheduled Logger every 300ms!", 300);
     scheduled_logger_start(logger2);
 
-    // we can also change the message and interval later if needed
+    // We can also change the message and interval later if needed:
     scheduled_logger_set_message(logger2, "Updated Message for Logger 2 every 200ms");
     scheduled_logger_set_interval(logger2, 200);
 }
